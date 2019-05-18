f114273c6b3b65d41fe207a8cf9efd6e8bd06f70
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-05-18 14:41:54 +0530

    added unspash images and updated preview image

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -8,6 +8,9 @@ export default function PageFooter() {
         <li>
           Design: <a href="http://html5up.net">HTML5 UP</a>
         </li>
+        <li>
+          Demo Images: <a href="http://unsplash.com/">Unsplash</a>
+        </li>
       </ul>
     </div>
   );
